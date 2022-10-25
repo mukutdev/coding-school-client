171c049ea96ec69e27a73d2ae47dd418751d6c55
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-10-25 21:52:51 +0600

    navlink-style-added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,54 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/logo.svg";
 
 const Header = () => {
+  const activeClass = {
+    backgroundColor: "#2563eb",
+    color: "#fff",
+  };
+
   return (
-    <div>
+    <div className="py-5 container mx-auto">
       <div className="flex justify-between">
         <div className="logo-wrapper">
-          <Link to={'/'}>Coding School</Link>
+          <Link to={"/"}>
+            <img src={logo} alt="" />
+          </Link>
+        </div>
+        <div className="ml-auto mr-16">
+          <nav className="flex justify-between">
+            <NavLink
+              style={({ isActive }) => (isActive ? activeClass : undefined)}
+              className="mr-3 py-3 px-6 uppercase hover:bg-gray-800 hover:text-white text-base tracking-widest font-medium rounded-lg"
+              to={"/courses"}
+            >
+              Courses
+            </NavLink>
+            <NavLink
+              style={({ isActive }) => (isActive ? activeClass : undefined)}
+              className="mr-3 py-3 px-6 uppercase hover:bg-gray-800 hover:text-white text-base tracking-widest font-medium rounded-lg"
+              to={"/blogs"}
+            >
+              Blogs
+            </NavLink>
+            <NavLink
+              style={({ isActive }) => (isActive ? activeClass : undefined)}
+              className="mr-3 py-3 px-6 uppercase hover:bg-gray-800 hover:text-white text-base tracking-widest font-medium rounded-lg"
+              to={"/faq"}
+            >
+              Faq
+            </NavLink>
+          </nav>
         </div>
         <div>
-          <nav className="flex justify-between">
-            <Link to={"/courses"}>Courses</Link>
-            <Link to={"/blogs"}>Blogs</Link>
-            <Link to={"/faq"}>Faq</Link>
-            <Link to={"/login"}>Login</Link>
-            <Link to={"/register"}>Register</Link>
-          </nav>
+          <div>
+            <Link to={"/login"}>
+              <button className="mr-3 py-3 px-6 uppercase bg-blue-600 hover:bg-gray-800 text-white text-base tracking-widest font-medium rounded-lg">
+                Login
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
