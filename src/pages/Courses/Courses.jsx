9368c7511f75138b61bdbba6dc5552a8a228bbd3
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-10-25 13:37:02 +0600

    folder-structure-created

---
+++ src/pages/Courses/Courses.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Courses = () => {
+    return (
+        <div>
+            this is courses
+        </div>
+    );
+};
+
+export default Courses;
---
+++ src/pages/Faq/Faq.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Faq = () => {
+    return (
+        <div>
+          this is faq  
+        </div>
+    );
+};
+
+export default Faq;
