d1044533683fde8cadd49b124b1a9f53975e2af7
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-10-27 19:19:11 +0600

    course-page-responsive-complete

--- src/components/BackgroundImage/BackgroundImage.jsx
+++ src/components/BackgroundImage/BackgroundImage.jsx
@@ -4,9 +4,9 @@ const BackgroundImage = ({ img, content }) => {
   return (
     <section
       style={{ backgroundImage: `url(${img})` }}
-      className="h-96 bg-cover flex items-center justify-center"
+      className="md:h-96 h-64 bg-cover flex items-center justify-center"
     >
-      <h2 className="text-4xl uppercase tracking-widest text-white font-semibold">
+      <h2 className="md:text-4xl text-2xl uppercase tracking-widest text-white font-semibold">
         {content}
       </h2>
     </section>
--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -14,25 +14,25 @@ const Courses = () => {
       ></BackgroundImage>
       <div style={{ backgroundColor: "#F3F7FB" }}>
         <div className="container mx-auto">
-          <div className="grid grid-cols-3 gap-12 py-10">
-            <div className="col-span-2">
-              <div className="grid grid-cols-2 gap-7">
+          <div className="grid md:grid-cols-3 gap-12 py-10">
+            <div className="md:col-span-2">
+              <div className="grid md:grid-cols-2 grid-cols-1 gap-7">
                 {courses.map(course => (
                   <CourseCard key={course.id} course={course}></CourseCard>
                 ))}
               </div>
             </div>
-            <div className="col-span-1">
+            <div className="md:col-span-1 mx-3">
               <div className="bg-white pt-5 px-8 rounded-lg sticky top-3">
                 <h2 className="text-2xl font-semibold text-center">
                   Most Purchased Courses
                 </h2>
                 <div className="flex flex-col gap-5 py-6">
                   {courses.map(course => (
-                    <div className="flex  gap-5 my-5" key={course.id}>
-                      <div>
+                    <div className="flex flex-col md:flex-row gap-5 my-5" key={course.id}>
+                      <div className="">
                         <img
-                          className="h-24 w-full rounded-lg"
+                          className="md:h-24  w-44 md:w-full rounded-lg"
                           src={course.thumb}
                           alt=""
                         />
